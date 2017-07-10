7694874d73472615b7d277ecace2d825cf0e9def
Daniel Castro <castrodd@gmail.com>
2017-07-09 22:18:18 -0400

    Indexed new sites.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,18 +1,30 @@
 class SitesController < ApplicationController
     def index
+      @sites = Site.last
     end
 
     def new
       @site = Site.new
     end
 
+    def show
+       @site = Site.last
+    end
+
     def create
-      @site = Site.create(params[:site])
+      @site = Site.create(site_params)
 
       if @site.save
-        redirect_to @site
+        redirect_to :action => "index"
       else
         redirect_to :action => "new"
       end
+    end
+
+    private
+
+    def site_params
+      allow = [:name]
+      params.require(:site).permit(allow)
     end
 end
