fe3174ac72a838b46266169d7153abd138490368
Daniel Castro <castrodd@gmail.com>
2017-07-11 13:50:40 -0400

    Fixed syntax error.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sites#new'
   get '/sites/', to: 'sites#new'
-  get '/:id', to 'sites#show'
+  get 'sites/:id', to 'sites#show'
 end
