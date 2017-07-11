93b8d979ba32f6f91a24c27b7864e3bd83b4988f
Daniel Castro <castrodd@gmail.com>
2017-07-11 14:14:26 -0400

    Resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sites#new'
-  get '/sites', to: 'sites#new'
-  get 'sites/:id', to: 'sites#show'
+  resources :sites, only: [:new, :show, :create]
 end
