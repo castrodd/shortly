54539b6fb4e17787d3c283df4d8f8a550653228b
Daniel Castro <castrodd@gmail.com>
2017-07-11 13:56:29 -0400

    Paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root new_site_path
-  get sites_path, to: new_site_path
+  root 'sites#new'
+  get '/sites', to: 'sites#new'
   get 'sites/:id', to: 'sites#show'
 end
